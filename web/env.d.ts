fdcad5aa18fe1166f4653540cca92c254f9b6598
nwleedev <nw.lee@outlook.com>
2025-01-30 09:39:15 +0900

    feat: web setup

---
+++ web/env.d.ts
@@ -0,0 +1,11 @@
+import "next";
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_API_URL: string;
+      NEXT_PUBLIC_VAPID_PUBLIC_KEY: string;
+      VAPID_PRIVATE_KEY: string;
+    }
+  }
+}
---
+++ web/next.config.mjs
@@ -0,0 +1,13 @@
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withSerwist(nextConfig);
