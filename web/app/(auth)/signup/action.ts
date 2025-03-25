d7d9e5fcefbfe89b07715b5f99ccd84aa3bf8e1d
nwleedev <nw.lee@outlook.com>
2025-03-25 09:42:28 +0900

    fix: create default preference when signup

--- web/app/(auth)/signup/action.ts
+++ web/app/(auth)/signup/action.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import { AuthSchemaLibs, SignUpProps } from "@/lib/auth/schema";
+import { getLocalCurrency } from "@/lib/currency";
 import { PocketBaseLibs } from "@/lib/pocket-base";
+import { getLocalTimezone } from "@/lib/timezone";
 
 export async function action(formData: SignUpProps) {
   const parsed = AuthSchemaLibs.signUp.parse(formData);
@@ -11,6 +13,13 @@ export async function action(formData: SignUpProps) {
   const model = await client
     .collection("users")
     .create({ email, password, name, passwordConfirm });
+  const preference = await client.collection("preferences").create({
+    user: model.id,
+    timezone: getLocalTimezone(),
+    timezoneOffset: new Date().getTimezoneOffset(),
+    hour: 9,
+    currency: getLocalCurrency(),
+  });
 
   return {
     message: "User created",
